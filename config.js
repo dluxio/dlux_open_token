102cb08fb13741bd73022ac862a893defa0a6ff5
disregardfiat <sjettingerjr@gmail.com>
2019-08-30 14:18:06 -0500

    ACTs

--- config.js
+++ config.js
@@ -1,23 +1,22 @@
 const ENV = process.env;
 
-const username = ENV.account || 'dlux-io';
-const active = ENV.active || '';
+const username = ENV.account || '';
+const activeKey = ENV.active || '';
 const memoKey = ENV.memo || '';
-const NODEDOMAIN = ENV.domain || 'http://dlux-token.herokuapp.com'
-const override = ENV.override || false
-const rta = ENV.rta || ''
-const rtp = ENV.rtp || ''
-
-const bidRate = ENV.BIDRATE || 2500
+const NODEDOMAIN = ENV.domain || '';
 const port = ENV.PORT || 3000;
-const clientURL = ENV.APIURL || 'https://api.steemit.com'
-
-
-const engineCrank = ENV.startingHash || 'QmWRJdREoTxYyUreshW4MCuttvxZaP7RUSGzDxLtdxyhWK'
-const acm = ENV.account_creator || false //account creation market
+const clientURL = ENV.API_URL || 'https://api.steemit.com'
+const ec = ENV.starting_hash || ''
+const auto = ENV.account_creator || true //automates purchasing ACTs with RCs
+const low = ENV.low_price || 1 //lowest sell price for ACTs
+const fee = ENV.escrow_fee || 1 //lowest escrow fee accepted
+const rrate = ENV.release_rate || 2000 //maximum percent of ACTs to redeem/sell per day
+const rcmin = ENV.rc_min || 2000 //lowest RC balance the auto ACT purchaser will achieve
+const fut = ENV.futures || false //willing to engage in ACT futures
+const futmax = ENV.futures_max || 31536000 //max time a futures contract will be held before liquidated in blocks
 
 let config = {
-    username,active,memoKey, NODEDOMAIN, bidRate, engineCrank, port, clientURL, acm, rta, rtp,override
+    username, activeKey, memoKey, NODEDOMAIN, ec, port, clientURL, agent, low, fee, rrate, rcmin, fut, futmax
 };
 
 module.exports = config;
