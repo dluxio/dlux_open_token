0a2db46ae2461a3dbbc02d1d4a8d8d0c265e0836
disregardfiat <sjettingerjr@gmail.com>
2019-09-06 17:32:42 -0500

    update bl

--- config.js
+++ config.js
@@ -9,15 +9,15 @@ const clientURL = ENV.API_URL || 'https://api.steemit.com'
 const ec = ENV.starting_hash || ''
 const auto = ENV.account_creator || true //automates purchasing ACTs with RCs
 const low = ENV.low_price || 1 //lowest sell price for ACTs
-const fee = ENV.escrow_fee || 1 //lowest escrow fee accepted
 const rrate = ENV.release_rate || 2000 //maximum percent of ACTs to redeem/sell per day
 const rcmin = ENV.rc_min || 2000 //lowest RC balance the auto ACT purchaser will achieve
-const fut = ENV.futures || false //willing to engage in ACT futures
-const futmax = ENV.futures_max || 31536000 //max time a futures contract will be held before liquidated in blocks
+const del = ENV.del || false //willing to delegate SP
+const delmax = ENV.del_max || 2500 //max percent of SP willing to delegate
 const delshare = ENV.del_share || true
+const bl = ENV.bl || 'https://raw.githubusercontent.com/themarkymark-steem/buildawhaleblacklist/master/blacklist.txt'
 
 let config = {
-    username, activeKey, memoKey, NODEDOMAIN, ec, port, clientURL, agent, low, fee, rrate, rcmin, fut, futmax, delshare
+    username, activeKey, memoKey, NODEDOMAIN, ec, port, clientURL, low, rrate, rcmin, del, delmax, delshare, bl
 };
 
 module.exports = config;
