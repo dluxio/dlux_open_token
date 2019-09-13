1e235f1fa8e1d2890404bb8f2881849609b8b89b
disregardfiat <sjettingerjr@gmail.com>
2019-09-13 07:53:53 -0700

    more config

--- config.js
+++ config.js
@@ -7,17 +7,19 @@ const NODEDOMAIN = ENV.domain || '';
 const port = ENV.PORT || 3000;
 const clientURL = ENV.API_URL || 'https://api.steemit.com'
 const ec = ENV.starting_hash || ''
+const ec_ovr = ENV.replay_ovr || 0
 const auto = ENV.account_creator || true //automates purchasing ACTs with RCs
 const low = ENV.low_price || 1 //lowest sell price for ACTs
 const rrate = ENV.release_rate || 2000 //maximum percent of ACTs to redeem/sell per day
 const rcmin = ENV.rc_min || 2000 //lowest RC balance the auto ACT purchaser will achieve
-const del = ENV.del || false //willing to delegate SP
+const autoclaim = ENV.auto_claim_act || true
+const del = ENV.del || true //willing to delegate SP
 const delmax = ENV.del_max || 2500 //max percent of SP willing to delegate
-const delshare = ENV.del_share || true
+const delshare = ENV.del_share || true //future implementation
 const bl = ENV.bl || 'https://raw.githubusercontent.com/themarkymark-steem/buildawhaleblacklist/master/blacklist.txt'
 
 let config = {
-    username, activeKey, memoKey, NODEDOMAIN, ec, port, clientURL, low, rrate, rcmin, del, delmax, delshare, bl
+    username, activeKey, memoKey, NODEDOMAIN, ec, port, clientURL, low, rrate, rcmin,autoclaim, del, delmax, delshare, bl
 };
 
 module.exports = config;
