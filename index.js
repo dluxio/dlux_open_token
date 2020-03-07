24588543c481c7d5492c4a90f875ac4d2bf1e1fd
Steven Ettinger <sjettingerjr@gmail.com>
2020-03-07 12:30:01 -0800

    ughhh

--- index.js
+++ index.js
@@ -1026,7 +1026,7 @@ function startApp() {
             });
         })
         var Pcontract = new Promise(function(resolve, reject) {
-            store.get(['contracts', seller, contract, function(e, a) {
+            store.get(['contracts', seller, contract], function(e, a) {
                 if (e) { resolve(0) } else if (isEmpty(a)) { resolve(0) } else { resolve(a) }
             });
         })
