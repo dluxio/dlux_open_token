37aa7f5fc5a17c9a7aed125f99ab51de2efac708
Steven Ettinger <sjettingerjr@gmail.com>
2020-03-07 11:58:53 -0800

    enhance

--- index.js
+++ index.js
@@ -1438,6 +1438,7 @@ function startApp() {
     processor.onOperation('escrow_release', function(json) {
         store.get(['escrow', json.escrow_id, json.from], function(e, a) { // since escrow ids are unique to sender, store a list of pointers to the owner of the contract
             if (!e) {
+                console.log(a)
                 store.get(['contracts', a.for, a.contract.split(':')[1]], function(e, b) {
                     if (e) { console.log(e1) } else {
                         console.log(json, b)
