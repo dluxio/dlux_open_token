f90cc3776898e6d9fc508e9a3605b365caa1a6a0
Steven Ettinger <sjettingerjr@gmail.com>
2020-04-15 08:50:15 -0500

    four

--- index.js
+++ index.js
@@ -677,10 +677,8 @@ function startApp() {
         Promise.all([fbalp, tbp])
             .then(bals => {
                 let fbal = bals[0],
-                    tb = bals[1],
-                    tbal,
+                    tbal = bals[1],
                     ops = []
-                var tbal = typeof tb != 'number' ? 0 : tb
                 if (json.to && typeof json.to == 'string' && typeof json.amount == 'number' && (json.amount | 0) === json.amount && json.amount >= 0 && fbal >= json.amount && active) {
                     ops.push({ type: 'put', path: ['balances', from], data: (fbal - json.amount) })
                     ops.push({ type: 'put', path: ['balances', json.to], data: (tbal + json.amount) })
