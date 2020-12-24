cde3b873bfec45b79254bccae532a3f7dc8f4e68
Steven Ettinger <sjettingerjr@gmail.com>
2020-12-24 18:52:05 -0300

    escrow is hard

--- index.js
+++ index.js
@@ -1013,8 +1013,8 @@ function startApp() {
             buy = contract.amount
             console.log(buy, isAgent, isDAgent)
             if (typeof buy === 'number' && isAgent && isDAgent && btime) { //{txid, from: from, buying: buyAmount, amount: json.dlux, [json.dlux]:buyAmount, rate:parseFloat((json.dlux)/(buyAmount)).toFixed(6), block:current, partial: json.partial || true
-                console.log(contract.hive, parseInt(parseFloat(json.hive_amount) * 1000), contract.hbd, contract.hbd, parseInt(parseFloat(json.hbd_amount) * 1000), check, until)
-                if (contract.hive == parseInt(parseFloat(json.hive_amount) * 1000) && contract.hbd == parseInt(parseFloat(json.hbd_amount) * 1000) && check > until) {
+                console.log(contract.hive, parseInt(parseFloat(json.hive_amount) * 1000), contract.hbd, contract.hbd, parseInt(parseFloat(json.hbd_amount) * 1000), check)
+                if (contract.hive == parseInt(parseFloat(json.hive_amount) * 1000) && contract.hbd == parseInt(parseFloat(json.hbd_amount) * 1000) && btime) {
                     console.log(1)
                     if (contract.hbd) { type = 'hbd' }
                     getPathObj(['dex', type, 'sellOrders'])
