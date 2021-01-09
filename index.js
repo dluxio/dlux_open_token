f55c5369f2e892f3a64a1529a0da398280aa9c51
Steven Ettinger <sjettingerjr@gmail.com>
2021-01-09 17:37:23 -0300

    not streaming?

--- index.js
+++ index.js
@@ -392,7 +392,26 @@ function startApp() {
                 })
             })
         });
-    processor.onStreamingStart(HR.onStreamingStart);
+    processor.onStreamingStart(() => {
+        console.log("At real time.");
+        store.get(['markets', 'node', config.username], function(e, a) {
+            if (!a.domain && config.NODEDOMAIN) {
+                var op = ["custom_json", {
+                    required_auths: [config.username],
+                    required_posting_auths: [],
+                    id: `${config.prefix}node_add`,
+                    json: JSON.stringify({
+                        domain: config.NODEDOMAIN,
+                        bidRate: config.bidRate,
+                        escrow: true
+                    })
+                }];
+                unshift([
+                    [0, 0], op
+                ]);
+            }
+        });
+    });
     processor.start();
 }
 
