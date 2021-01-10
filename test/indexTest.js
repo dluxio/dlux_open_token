9601635a67c7bda25c21f11cc73d8a1b91141f97
Steven Ettinger <sjettingerjr@gmail.com>
2021-01-10 05:11:28 -0300

    unit tests!

--- test/indexTest.js
+++ test/indexTest.js
@@ -21,6 +21,46 @@ describe('State', function() {
             .then(res => assert.equal(res, true))
     })
 
+    it('Add test node A:', () => {
+        let json = {
+            domain: 'localhost',
+            bidRate: 2001,
+            marketingRate: -1,
+            escrow: true,
+            block_num: 1,
+            transaction_id: 1
+        }
+        return new Promise((resolve, reject) => {
+                app.node_add(json, 'node-opa', true, [resolve, reject])
+            })
+            .then(ops => {
+                assert.equal(ops[0].path[2], 'node-opa')
+                assert.equal(ops[0].data.domain, 'localhost')
+                assert.equal(ops[0].data.bidRate, 2000)
+                assert.equal(ops[0].data.marketingRate, 0)
+            })
+    })
+
+    it('Add test node B:', () => {
+        let json = {
+            domain: 'localhost',
+            bidRate: 1000,
+            marketingRate: 'shrimp',
+            escrow: true,
+            block_num: 1,
+            transaction_id: 1
+        }
+        return new Promise((resolve, reject) => {
+                app.node_add(json, 'node-opb', true, [resolve, reject])
+            })
+            .then(ops => {
+                assert.equal(ops[0].path[2], 'node-opb')
+                assert.equal(ops[0].data.domain, 'localhost')
+                assert.equal(ops[0].data.bidRate, 1000)
+                assert.equal(ops[0].data.marketingRate, 0)
+            })
+    })
+
     it('Testing send:', () => {
         let json = {
             to: 'test-to',
@@ -39,10 +79,46 @@ describe('State', function() {
             })
     })
 
+    it('Liquidizing Node A:', () => {
+        let json = {
+            to: 'node-opa',
+            amount: 90000,
+            block_num: 1,
+            transaction_id: 1
+        }
+        return new Promise((resolve, reject) => {
+                app.send(json, 'test-from', true, [resolve, reject])
+            })
+            .then(ops => {
+                assert.equal(ops[0].path[1], 'test-from')
+                assert.equal(ops[1].path[1], 'node-opa')
+                assert.equal(ops[1].data, 90000)
+                assert.equal(ops[0].data, 909000)
+            })
+    })
+
+    it('Liquidizing Node B:', () => {
+        let json = {
+            to: 'node-opb',
+            amount: 9000,
+            block_num: 1,
+            transaction_id: 1
+        }
+        return new Promise((resolve, reject) => {
+                app.send(json, 'test-from', true, [resolve, reject])
+            })
+            .then(ops => {
+                assert.equal(ops[0].path[1], 'test-from')
+                assert.equal(ops[1].path[1], 'node-opb')
+                assert.equal(ops[1].data, 9000)
+                assert.equal(ops[0].data, 900000)
+            })
+    })
+
     it('Testing over send:', () => {
         let json = {
             to: 'test-to',
-            amount: 1000000,
+            amount: 900001,
             block_num: 1,
             transaction_id: 1
         }
@@ -98,4 +174,5 @@ describe('State', function() {
                 assert.equal(ops[0].data, '@test-from| Invalid send operation')
             })
     })
+
 })
