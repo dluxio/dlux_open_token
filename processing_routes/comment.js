e73688a942dd54fb2341886446485d5bd0ec7a61
Steven Ettinger <sjettingerjr@gmail.com>
2021-01-14 01:36:30 -0300

    pob

--- processing_routes/comment.js
+++ processing_routes/comment.js
@@ -1,11 +1,13 @@
 const config = require('./../config')
 const { rtrades } = require('./../rtrades')
-const { store, client, unshiftOp } = require('./../index')
+const { store, unshiftOp } = require('./../index')
 const { deleteObjs } = require('./../deleteObjs')
 const { chronAssign } = require('./../lil_ops')
 
 exports.comment = (json, pc) => {
-    if (json.author == config.leader) {
+    let meta = JSON.parse(json.json_metadata)
+    let community_post = false
+    if (json.author == config.leader && parseInt(json.permlink.split('dlux')[1]) > json.block_num - 31000) {
         store.get(['escrow', json.author], function(e, a) {
             if (!e) {
                 var ops = []
@@ -14,11 +16,70 @@ exports.comment = (json, pc) => {
                         ops.push({ type: 'del', path: ['escrow', json.author, b] })
                     }
                 }
+                if (process.env.npm_lifecycle_event == 'test') pc[2] = ops
                 store.batch(ops, pc)
             } else {
                 console.log(e)
             }
         })
+    } else if (meta.arHash || meta.vrHash || meta.appHash || meta.audHash) {
+        //store json until a vote or promote with comment options
+        var ops = []
+        ops.push({
+            type: 'put',
+            path: ['pend', `${json.author}/${json.permlink}`],
+            data: {
+                author: json.author,
+                permlink: json.permlink,
+                block_num: json.block_num,
+                meta
+            }
+        })
+        ops.push({
+            type: 'put',
+            path: ['chrono', `${json.block_num}:pend:${json.author}/${json.permlink}`],
+            data: {
+                author: json.author,
+                permlink: json.permlink,
+                block_num: json.block_num,
+                meta
+            }
+        })
+        if (process.env.npm_lifecycle_event == 'test') pc[2] = ops
+        store.batch(ops, pc)
+            /*
+                }
+                
+
+                    //tag search for -LEO Community
+                    for (tag in meta.tags) {
+                        if (community_post) { break; }
+                        for (i = 0; i < config.community_tags.length; i++) {
+                            if (tag == config.community_tags[i]) {
+                                community_post = true
+                                break;
+                            }
+                        }
+                    }
+
+                    
+                if (community_post) {
+
+                    //tag picker only -LEO Community
+                    var exp_path = chronAssign(json.block_num + 201600, { op: 'post_reward', a: json.author, p: json.permlink })
+                    promies.all([exp_path])
+                        .then(r => {
+                            const post = {
+                                author: json.author,
+                                permlink: json.permlink,
+                                expire_path: r[0],
+                                block: json.block_num
+                            }
+                            var ops = [{ type: 'put', path: ['posts', json.author, json.permlink], data: post }]
+                            store.batch(ops, pc)
+                        })
+                        .catch(e => console.log(e))
+            */
     } else {
         pc[0](pc[2])
     }
@@ -34,116 +95,49 @@ exports.comment_options = (json, pc) => {
     var ops = []
     for (var i = 0; i < filter.length; i++) {
         if (filter[i].account == config.ben && filter[i].weight >= config.delegationWeight) {
-            store.get(['queue'], function(e, a) {
-                if (e)
-                    console.log(e)
-                var queue = []
-                for (numb in a) {
-                    queue.push(numb)
-                }
-                chronAssign(json.block_num + 144000, {
-                    block: parseInt(json.block_num + 144000),
-                    op: 'post_reward',
-                    author: json.author,
-                    permlink: json.permlink
-                })
-                var assignments = [0, 0, 0, 0]
-                if (config.username == config.leader) { //pin content ... hard set here since rewards are still hard set as well
-                    assignments[0] = config.username
-                }
-                if (!e) {
-                    assignments[1] = queue.shift() //consensus accounts for API retrivals
-                    assignments[2] = queue.shift()
-                    assignments[3] = queue.shift()
-                    queue.push(assignments[1])
-                    queue.push(assignments[2])
-                    queue.push(assignments[3])
-                }
-                ops.push({
+            store.get(['pend', `${json.author}/${json.permlink}`], function(e, a) {
+                if (e) { console.log(e) }
+                if (a.permlink = json.permlink) {
+                    chronAssign(json.block_num + 201600, {
+                        block: parseInt(json.block_num + 201600),
+                        op: 'post_reward',
+                        author: json.author,
+                        permlink: json.permlink
+                    })
+                    ops.push({
                         type: 'put',
                         path: ['posts', `${json.author}/${json.permlink}`],
                         data: {
                             block: json.block_num,
                             author: json.author,
                             permlink: json.permlink,
-                            totalWeight: 1,
-                            voters: {},
-                            reblogs: {},
-                            credentials: {},
-                            signatures: {},
-                            customJSON: {
-                                assignments: [config.leader, assignments[1], assignments[2], assignments[3]]
-                            },
+                            customJSON: a.meta
                         }
                     })
-                    //ops.push({ type: 'put', path: ['queue'], data: queue })
-                ops.push({ type: 'put', path: ['feed', `${json.block_num}:${json.transaction_id}`], data: `@${json.author}|${json.permlink} added to ${config.TOKEN} rewardable content` })
-                store.batch(ops, pc)
-                if (assignments[0] == config.username || assignments[1] == config.username || assignments[2] == config.username || assignments[3] == config.username) {
-                    client.database.call('get_content', [json.author, json.permlink])
-                        .then(result => {
-                            console.log('hive content', result)
-                            var trimmed = JSON.parse(result.json_metadata),
-                                final = { a: [] }
-                            for (j in trimmed.assets) {
-                                if (trimmed.assets[j].hash.length == 46)
-                                    final.a.push(trimmed.assets[j].hash) //a for assets
-                            }
-                            if (trimmed.app.length < 33) { //p for process
-                                final.p = trimmed.app
-                            }
-                            try {
-                                if (trimmed.Hash360.length == 46) { //s for surround
-                                    final.s = trimmed.Hash360
-                                }
-                            } catch (e) {}
-                            try {
-                                if (trimmed.vrHash.length == 46) { //e for executable
-                                    final.e = trimmed.vrHash
-                                }
-                            } catch (e) {}
-                            try {
-                                if (JSON.stringify(trimmed.loc).length < 1024) { //l for spactial indexing
-                                    final.l = trimmed.loc
-                                }
-                            } catch (e) {}
-                            final.t = trimmed.tags
-                            final.d = result.title
-                            if (assignments[0]) { //mirror username will need rtrades login
-                                var bytes = rtrades.checkNpin(JSON.parse(result.json_metadata)
-                                    .assets)
-                                bytes.then(function(value) {
-                                    var op = ["custom_json", {
-                                        required_auths: [config.username],
-                                        required_posting_auths: [],
-                                        id: `${config.prefix}cjv`,
-                                        json: JSON.stringify({
-                                            a: json.author,
-                                            p: json.permlink,
-                                            c: final,
-                                            b: value //amount of bytes posted
-                                        })
-                                    }]
-                                    unshiftOp([
-                                        [0, 0], op
-                                    ])
-                                }).catch(e => { console.log(e) })
-                            } else {
-                                var op = ["custom_json", {
-                                    required_auths: [config.username],
-                                    required_posting_auths: [],
-                                    id: `${config.prefix}cjv`,
-                                    json: JSON.stringify({
-                                        a: json.author,
-                                        p: json.permlink,
-                                        c: final
-                                    })
-                                }]
-                                unshiftOp([
-                                    [0, 0], op
-                                ])
-                            }
-                        }).catch(e => { console.log(e) })
+                    if (config.username == config.leader) {
+                        var bytes = rtrades.checkNpin(a.meta.assets)
+                        bytes.then(function(value) {
+                            var op = ["custom_json", {
+                                required_auths: [config.username],
+                                required_posting_auths: [],
+                                id: `${config.prefix}cjv`,
+                                json: JSON.stringify({
+                                    a: json.author,
+                                    p: json.permlink,
+                                    c: final,
+                                    b: value //amount of bytes posted
+                                })
+                            }]
+                            unshiftOp([
+                                [0, 0], op
+                            ])
+                        })
+                    }
+                    ops.push({ type: 'del', path: ['pend', `${json.author}/${json.permlink}`] })
+                    ops.push({ type: 'del', path: ['chrono', `${a.block_num}:pend:${json.author}/${json.permlink}`] })
+                    ops.push({ type: 'put', path: ['feed', `${json.block_num}:${json.transaction_id}`], data: `@${json.author}|${json.permlink} added to ${config.TOKEN} rewardable content` })
+                    if (process.env.npm_lifecycle_event == 'test') pc[2] = ops
+                    store.batch(ops, pc)
                 }
             })
         } else {
@@ -151,3 +145,73 @@ exports.comment_options = (json, pc) => {
         }
     }
 }
+
+/*
+processor.onOperation('comment', function(json, pc) { //grab posts to reward
+        let meta = JSON.parse(json.json_metadata)
+        let community_post = false
+        //store meta data, promote post if comment options with bens, demote if vote.
+        if (meta.arHash || meta.vrHash || meta.appHash || meta.audHash){
+            
+        }
+
+        /* //tag search for -LEO Community
+        for (tag in meta.tags) {
+            if (community_post) { break; }
+            for (i = 0; i < config.community_tags.length; i++) {
+                if (tag == config.community_tags[i]) {
+                    community_post = true
+                    break;
+                }
+            }
+        }
+if (community_post) {
+
+ //tag picker only -LEO Community
+    var exp_path = chronAssign(json.block_num + 201600, { op: 'post_reward', a: json.author, p: json.permlink })
+    promies.all([exp_path])
+        .then(r => {
+            const post = {
+                author: json.author,
+                permlink: json.permlink,
+                expire_path: r[0],
+                block: json.block_num
+            }
+            var ops = [{ type: 'put', path: ['posts', json.author, json.permlink], data: post }]
+            store.batch(ops, pc)
+        })
+        .catch(e => console.log(e))
+        
+} else {
+    pc[0](pc[2])
+}
+
+if (json.author == config.leader) { //clear auto-voter
+    store.get(['escrow', json.author], function(e, a) {
+        if (!e) {
+            var ops = []
+            for (b in a) {
+                if (a[b][1].permlink == json.permlink && b == 'comment') {
+                    ops.push({ type: 'del', path: ['escrow', json.author, b] })
+                    if (json.author == config.username) {
+                        for (var i = 0; i < NodeOps.length; i++) {
+                            if (NodeOps[i][1][1].permlink == json.permlink && NodeOps[i][1][0] == 'comment') {
+                                NodeOps.splice(i, 1)
+                            }
+                        }
+                        delete plasma.pending[b]
+                    }
+                    break;
+                }
+            }
+            store.batch(ops, pc)
+        } else {
+            console.log(e)
+        }
+    })
+} else {
+    pc[0](pc[2])
+}
+
+});
+*/
