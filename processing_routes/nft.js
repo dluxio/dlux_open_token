8998f37c11ef12f4afe98e9c6b2b0486784d3dd4
Steven Ettinger <sjettingerjr@gmail.com>
2021-10-14 23:32:35 -0300

    delete melt

--- processing_routes/nft.js
+++ processing_routes/nft.js
@@ -162,8 +162,8 @@ exports.nft_delete  = function(json, from, active, pc) {
     Promise.all([fnftp, setp])
     .then(nfts => {
         if(nfts[0].s && !nfts[0].l && active) {
-            var ops = []
-                nft = nfts[0],
+            let ops = [],
+                //nft = nfts[0],
                 set = nfts[1]
             set.u = NFT.delete(json.uid, set.u)
             if(set.d)set.d++
