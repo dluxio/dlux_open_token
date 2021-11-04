2cd0ab90dce40da3c7bd47478e5dc74325523d34
Steven Ettinger <sjettingerjr@gmail.com>
2021-11-03 22:02:42 -0300

    ketchup

--- config.js
+++ config.js
@@ -29,8 +29,8 @@ var ipfshost = ENV.ipfshost || 'ipfs.infura.io' //IPFS upload/download provider 
 const bidRate = ENV.BIDRATE || 2500 //
 
 //HIVE CONFIGS
-var startURL = ENV.STARTURL || "https://rpc.ecency.com/"
-var clientURL = ENV.APIURL || "https://hived.emre.sh/"
+var startURL = ENV.STARTURL || "https://hived.emre.sh/"
+var clientURL = ENV.APIURL || startURL
 const clients = ENV.clients || [
     "https://api.deathwing.me/",
         //"https://rpc.ecency.com/",
