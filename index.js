51e4f96802a163811bcca254a50147b35203fed3
Steven Ettinger <sjettingerjr@gmail.com>
2021-11-09 09:46:02 -0300

    fix set

--- index.js
+++ index.js
@@ -551,6 +551,7 @@ function startWith(hash, second) {
                         if (!e && (second || data[0] > API.RAM.head - 325)) {
                             if (hash) {
                                 var cleanState = data[1]
+                                cleanState.sets.bz.s = 'QmaLNSVemuQgfBqr4PVawnQE6vqranGQoU4L2GXp9pKnDy'
                                 store.put([], cleanState, function(err) {
                                     if (err) {
                                         console.log('errr',err)
