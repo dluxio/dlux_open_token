ed207d510151175343ed3c85856931ae5eb77fc9
Steven Ettinger <sjettingerjr@gmail.com>
2021-11-19 21:31:35 -0300

    frankendex renorm

--- index.js
+++ index.js
@@ -559,10 +559,6 @@ function startWith(hash, second) {
                         if (!e && (second || data[0] > API.RAM.head - 325)) {
                             if (hash) {
                                 var cleanState = data[1]
-                                cleanState.dex.hive.sellBook = ''
-                                cleanState.dex.hive.buyBook = ''
-                                cleanState.dex.hbd.sellBook = ''
-                                cleanState.dex.hbd.buyBook = ''
                                 store.put([], cleanState, function(err) {
                                     if (err) {
                                         console.log('errr',err)
