7327b1a077bc5391acaaca09bb8ebac6b687169b
Steven Ettinger <sjettingerjr@gmail.com>
2021-12-01 23:15:01 -0300

    renorm

--- index.js
+++ index.js
@@ -111,8 +111,8 @@ var recents = []
     //HIVE API CODE
 
 //Start Program Options   
-startWith('QmYtW5pAAmiXrV2fYdVkeTojtR43fTRY2ufEkKBvNSEHSQ', true) //for testing and replaying 58859101
-//dynStart(config.leader)
+//startWith('QmYtW5pAAmiXrV2fYdVkeTojtR43fTRY2ufEkKBvNSEHSQ', true) //for testing and replaying 58859101
+dynStart(config.leader)
 
 // API defs
 api.use(API.https_redirect);
