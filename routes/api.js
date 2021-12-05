c69ddc31ce9ba330ef6634e9f2d1751eba618eee
Steven Ettinger <sjettingerjr@gmail.com>
2021-12-05 02:25:52 -0300

    max qty api

--- routes/api.js
+++ routes/api.js
@@ -1156,6 +1156,7 @@ exports.mint_supply = (req, res, next) => {
                 }
                 let token = hivesells[item].h ? 'HIVE' : 'HBD'
                 let amount = hivesells[item].h ? hivesells[item].h : hivesells[item].b
+                let max = hivesells[item].e ? ( hivesells[item].e.split('max:')[1] ? hivesells[item].e.split('max:')[1].split(',')[0] : hivesells[item].q ) : hivesells[item].q
                 const listing = {
                     uid: item.split(':')[1],
                     set: item.split(':')[0],
@@ -1167,7 +1168,8 @@ exports.mint_supply = (req, res, next) => {
                         token: token
                     },
                     by:hivesells[item].o,
-                    script: mem[1][item.split(':')[0]].s
+                    script: mem[1][item.split(':')[0]].s,
+                    max
                 }
                 sets[item.split(':')[0]].qty += (hivesells[item].q || 1)
                 sets[item.split(':')[0]].qty_sales += (hivesells[item].q || 1)
