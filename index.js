ec01f872661df76a0e5a60d1044aaed986ea91f1
Steven Ettinger <sjettingerjr@gmail.com>
2021-12-07 13:48:12 -0300

    mmm

--- index.js
+++ index.js
@@ -607,7 +607,6 @@ function startWith(hash, second) {
                         if (!e && (second || data[0] > API.RAM.head - 325)) {
                             if (hash) {
                                 var cleanState = data[1]
-                                delete cleanState.dex.hbd.buyOrders['1:DLUXQmXRtf6kj2GzgVzdZBe4sTXxLTsah5R3H6H3XSxiKfmZPf']
                                 store.put([], cleanState, function(err) {
                                     if (err) {
                                         console.log('errr',err)
