a5f352c8f86124de394ad8b90b80c10b69700e35
Steven Ettinger <sjettingerjr@gmail.com>
2021-12-09 20:42:51 -0300

    fix dex sell

--- index.js
+++ index.js
@@ -607,6 +607,7 @@ function startWith(hash, second) {
                         if (!e && (second || data[0] > API.RAM.head - 325)) {
                             if (hash) {
                                 var cleanState = data[1]
+                                cleanState.pcon.lth['hf:QmYwz8J5UpvfGQfUQbRqTyQzymWN6ue4epkC42TtPreJzL'].xawi = 1
                                 store.put([], cleanState, function(err) {
                                     if (err) {
                                         console.log('errr',err)
