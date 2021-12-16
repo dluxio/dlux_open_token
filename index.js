730c0d2376c4a4b4f7fec547733ec066b195aab2
Steven Ettinger <sjettingerjr@gmail.com>
2021-12-16 17:51:32 -0300

    renorm

--- index.js
+++ index.js
@@ -123,8 +123,8 @@ var recents = []
     //HIVE API CODE
 
 //Start Program Options   
-startWith('QmdC5Rt3qFkGsw3EbotfynkjZCNuxS5Ta3yeiKew1UkjPh', true) //for testing and replaying 58859101
-//dynStart(config.leader)
+//startWith('QmdC5Rt3qFkGsw3EbotfynkjZCNuxS5Ta3yeiKew1UkjPh', true) //for testing and replaying 58859101
+dynStart(config.leader)
 
 // API defs
 api.use(API.https_redirect);
@@ -619,21 +619,6 @@ function startWith(hash, second) {
                         if (!e && (second || data[0] > API.RAM.head - 325)) {
                             if (hash) {
                                 var cleanState = data[1]
-                                cleanState.chrono['60096745:divtest'] ={
-                                    set: 'hf',
-                                    op: 'div'
-                                }
-                                cleanState.dex.hive.buyBook = cleanState.dex.hive.buyBook.substr(1)
-                                // let str = '', current = ''
-                                // for (var order in cleanState.dex.hive.sellOrders){
-                                //     if(current == order.split(':')[0]){
-                                //         str = `${str}_${order.split(":")[1]}`
-                                //     } else {
-                                //         current = order.split(':')[0]
-                                //         str += `,${current}_${order.split(":")[1]}`
-                                //     }
-                                // }
-                                // cleanState.dex.hive.sellBook = str.substr(1)
                                 store.put([], cleanState, function(err) {
                                     if (err) {
                                         console.log('errr',err)
