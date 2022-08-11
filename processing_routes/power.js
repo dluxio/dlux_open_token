d7bb260508dd85f793b051949d9c4d8dfa0c0364
Steven Ettinger <sjettingerjr@gmail.com>
2022-08-11 17:42:12 -0300

    1.2.8r

--- processing_routes/power.js
+++ processing_routes/power.js
@@ -114,10 +114,26 @@ exports.power_grant = (json, from, active, pc) => {
                     ops.push({ type: 'put', path: ['granted', to, from], data: granted_to_from - less });
                     ops.push({ type: 'put', path: ['granted', to, 't'], data: granted_to_total - less });
                     ops.push({ type: 'put', path: ['pow', from], data: from_power + less });
-                    ops.push({ type: 'put', path: ['up', from], data: up_from });
-                    ops.push({ type: 'put', path: ['down', from], data: down_from });
-                    ops.push({ type: 'put', path: ['up', to], data: up_to });
-                    ops.push({ type: 'put', path: ['down', to], data: down_to });
+                    if (Object.keys(up_from).length)
+                      ops.push({
+                        type: "put",
+                        path: ["up", from],
+                        data: up_from,
+                      });
+                    if (Object.keys(down_from).length)
+                      ops.push({
+                        type: "put",
+                        path: ["down", from],
+                        data: down_from,
+                      });
+                    if (Object.keys(up_to).length)
+                      ops.push({ type: "put", path: ["up", to], data: up_to });
+                    if (Object.keys(down_to).length)
+                      ops.push({
+                        type: "put",
+                        path: ["down", to],
+                        data: down_to,
+                      });
                     const msg = `@${from}| Has granted ${parseFloat(amount/1000).toFixed(3)} to ${to}`
                     if (config.hookurl || config.status) postToDiscord(msg, `${json.block_num}:${json.transaction_id}`)
                     ops.push({ type: 'put', path: ['feed', `${json.block_num}:${json.transaction_id}`], data: msg });
@@ -131,6 +147,7 @@ exports.power_grant = (json, from, active, pc) => {
                 if (config.hookurl || config.status) postToDiscord(msg, `${json.block_num}:${json.transaction_id}`)
                 ops.push({ type: 'put', path: ['feed', `${json.block_num}:${json.transaction_id}`], data: msg });
             }
+            console.log(ops)
             store.batch(ops, pc);
         })
         .catch(e => { console.log(e); });
